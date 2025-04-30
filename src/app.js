5d4f9f25e64fb45bd33ceea940e15de7eb95f8d1
antruc <antruc@localhost.localdomain>
2025-04-29 20:30:29 -0700

    Reorganize images

--- src/app.js
+++ src/app.js
@@ -6,6 +6,6 @@ if (
   window.matchMedia('(prefers-color-scheme: dark)').matches
 ) {
   document.body.classList.add('dark')
-  document.querySelector('#logo').src = './logo-dark.svg'
-  document.querySelector('link[rel=icon]').href = './favicon-dark.png'
+  document.querySelector('#logo').src = './img/logo-dark.svg'
+  document.querySelector('link[rel=icon]').href = './img/favicon-dark.png'
 }

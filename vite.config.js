3c48791a3048e405ea00143602ab9f83f7feb9e5
antruc <admin@antruc.dev>
2026-05-15 13:35:54 -0600

    Format with Prettier

--- vite.config.js
+++ vite.config.js
@@ -1,18 +1,5 @@
-import { resolve } from 'path';
-import { defineConfig } from 'vite';
+import { defineConfig } from 'vite'
 
 export default defineConfig({
-  publicDir: 'public',
-  css: {
-    devSourcemap: true,
-  },
-  build: {
-    outDir: 'dist',
-    emptyOutDir: true,
-    rollupOptions: {
-      input: {
-        index:     resolve(__dirname, 'index.html'),
-      },
-    },
-  },
-});
+  base: '/'
+})
